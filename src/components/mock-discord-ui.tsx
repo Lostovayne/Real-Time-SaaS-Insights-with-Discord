daccbbdb933f7caeeb5a5f6cc7a32c596ca51520
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-05 22:48:20 -0300

    feat:

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react"
 import { Icons } from "./icons"
+import { PlusCircle, UserCircle } from "lucide-react"
 
 export const MockDiscordUi = ({ children }): PropsWithChildren => {
   return (
@@ -20,6 +21,29 @@ export const MockDiscordUi = ({ children }): PropsWithChildren => {
             </span>
           </div>
         ))}
+
+        <div className="group mt-auto size-12 bg-discord-background rounded-3xl flex items-center justify-center mb-3 hover:rounded-xl transition-all duration-200 hover:bg-[#3ba55c] cursor-not-allowed">
+          <PlusCircle className="text-[#3ba55c] group-hover:text-white" />
+        </div>
+      </div>
+
+      {/* dm list */}
+
+      <div className="hidden md:flex w-60 bg-[#2f3136] flex-col">
+        <div className="px-4 h-16 border-b border-[#202225] flex items-center shadow-sm">
+          <div className="w-full bg-[#202225] text-sm rounded px-2 h-8 flex items-center justify-center text-gray-500 cursor-not-allowed">
+            Find or start a conversation
+          </div>
+        </div>
+
+        <div className="flex-1 overflow-y-auto pt-4">
+          <div className="px-2 mb-4">
+            <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddde] cursor-not-allowed">
+              <UserCircle className="mr-4 size-8 text-[#b9bbbe]" />
+              <span className="font-medium text-sm">Friends</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
