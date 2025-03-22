9c55db5d45e8b85a5416507cd8698a46126bdabc
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-22 05:30:39 -0300

    style(landing): remove extra whitespace and update text for consistency
    Remove unnecessary whitespace in the landing page layout and update the notification text for clarity and consistency across devices

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -120,7 +120,6 @@ const Page = () => {
             {/* first bento grid element */}
             <div className="relative lg:row-span-2">
               <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
-
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
                 <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                   <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
@@ -128,7 +127,7 @@ const Page = () => {
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Get notified about critical events the moment they happen,
-                    no matter if you're at home or on the go.
+                    no matter if you&apos;re on your phone or your computer.
                   </p>
                 </div>
 
@@ -203,7 +202,10 @@ const Page = () => {
             </div>
 
             {/* Fourth bento grid element */}
-            <div className="relative lg:row-span-2"></div>
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]"></div>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
