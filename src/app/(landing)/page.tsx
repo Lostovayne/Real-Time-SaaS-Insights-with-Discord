d9d34dd5f06a5a0bd115afb0ece1281fd703f0f4
LostoVayne <moises.barrera.dev@gmail.com>
2025-04-24 23:54:03 -0400

    refactor(landing): reorganize customer reviews and add CTA button
    - Moved the second customer review into the same grid container as the first for better structure
    - Updated review content and user details for consistency
    - Added a "Start For Free Today" CTA button at the end of the section to improve user engagement

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -283,8 +283,9 @@ const Page = () => {
             </h2>
             <Heading className="text-center">What our customers say</Heading>
           </div>
-          {/* First Customer Review */}
+
           <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 divide-y lg:divide-y-0 lg:divide-x divide-gray-200">
+            {/* First Customer Review */}
             <div className="flex flex-auto flex-col gap-4 bg-brand-25 p-6 sm:p-8 lg:p-16 rounded-t-[2rem] lg:rounded-tr-none lg:rounded-l-[2rem]">
               <div className="flex gap-0.5 mb-2 justify-center lg:justify-start">
                 <Star className="size-5 text-brand-600 fill-brand-600" />
@@ -314,10 +315,8 @@ const Page = () => {
                 </div>
               </div>
             </div>
-          </div>
-          {/* Second Customer Review */}
-          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 divide-y lg:divide-y-0 lg:divide-x divide-gray-200">
-            <div className="flex flex-auto flex-col gap-4 bg-brand-25 p-6 sm:p-8 lg:p-16 rounded-t-[2rem] lg:rounded-tr-none lg:rounded-l-[2rem]">
+            {/* Second Customer Review */}
+            <div className="flex flex-auto flex-col gap-4 bg-brand-25 p-6 sm:p-8 lg:p-16 rounded-b-[2rem] lg:rounded-bl-none lg:rounded-r-[2rem]">
               <div className="flex gap-0.5 mb-2 justify-center lg:justify-start">
                 <Star className="size-5 text-brand-600 fill-brand-600" />
                 <Star className="size-5 text-brand-600 fill-brand-600" />
@@ -326,27 +325,34 @@ const Page = () => {
                 <Star className="size-5 text-brand-600 fill-brand-600" />
               </div>
               <p className="text-base sm:text-lg lg:text-lg/8 font-medium tracking-tight text-brand-950 text-center lg:text-left text-pretty">
-                PingPanda has been a game-changer for me. I&apos;ve been using
-                it for, two months now and it&apos;s been a lifesaver.
+                PingPanda&apos;s been paying off for our SaaS. Nice to have
+                simple way to see how we&apos;re doing day-to-day. Definitely
+                make our lives easier.
               </p>
               <div className="flex flex-col justify-center lg:justify-start sm:flex-row items-center sm:items-start gap-4 mt-2">
                 <Image
-                  src={"/user-2.png"}
-                  alt="Random User"
+                  src={"/user-1.png"}
+                  alt="Random User 1"
                   width={48}
                   height={48}
                   className="rounded-full object-cover size-12"
                 />
                 <div className="flex flex-col items-center sm:items-start">
                   <p className="font-semibold flex items-center">
-                    Freya Larsson
+                    Kai Durant
                     <Icons.verificationBadge className="size-4 inline-block ml-1.5" />
                   </p>
-                  <p className="text-sm text-gray-600">@itsfreya</p>
+                  <p className="text-sm text-gray-600">@kdurant_</p>
                 </div>
               </div>
             </div>
           </div>
+          <ShinyButton
+            href="/sign-up"
+            className="relative z-10 h-14 w-full max-w-xs text-base shadow-lg transition-shadow duration-300 hover:shadoe-xl"
+          >
+            Start Fot Free Today
+          </ShinyButton>
         </MaxWidthWrapper>
       </section>
     </>
